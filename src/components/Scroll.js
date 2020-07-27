f6e8d189ddab7ad26568b9e1955b8fb3a680721d
Venky-coder <61134886+Venky-coder@users.noreply.github.com>
2020-07-27 15:43:05 +0530

    Scroll.js file added in components

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Scroll = (props) =>
 {
-	console.log(props);
 	return (
 		<div style={{overflowY: 'scroll' , border: '5px solid black' , height: '800px'}}>
 		{props.children}
